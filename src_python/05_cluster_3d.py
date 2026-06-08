e48c7dba9f6f2f6fa27871867eb82f59b33926f2
nizaraflah <nizar.aflah@mhs.unsoed.ac.id>
2026-06-08 23:10:40 +0700

    Update 05_cluster_3d.py
    Program untuk menentukan apakah suatu titik tergolong cluster A, B, atau C.

--- src_python/05_cluster_3d.py
+++ src_python/05_cluster_3d.py
@@ -1,1 +1,24 @@
+import math
 
+def cluster_titik(x1, x2, x3):
+
+    A = (2, 1, 3)
+    B = (1, -4, 6)
+    C = (-2, 3, -2)
+
+    U = (x1, x2, x3)
+
+    dA = math.sqrt(sum((u - a) ** 2 for u, a in zip(U, A)))
+    dB = math.sqrt(sum((u - b) ** 2 for u, b in zip(U, B)))
+    dC = math.sqrt(sum((u - c) ** 2 for u, c in zip(U, C)))
+
+    if dA <= dB and dA <= dC:
+        print("Titik termasuk Cluster A")
+    elif dB <= dA and dB <= dC:
+        print("Titik termasuk Cluster B")
+    else:
+        print("Titik termasuk Cluster C")
+
+
+# Contoh penggunaan
+cluster_titik(1, 2, 3)
