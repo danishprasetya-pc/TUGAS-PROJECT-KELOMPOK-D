22ed8645c11ec175b260321a0be0eda0c66d2e4c
ergadewi-source <erga.dewi@mhs.unsoed.ac.id>
2026-06-09 10:34:52 +0700

    Update 02_string_kalimat.py

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -1,1 +1,10 @@
+#Soal 2 Membuat Objek String K1 sampai K4
+K1="Saya tak 'kan menyerah."
+K2='Ia berkata, "Aku menyayangimu."'
+K3="\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+K4="Surat keputusan itu bernomor 62/UN.34/19/2023."
 
+print(K1)
+print(K2)
+print(K3)
+print(K4)
