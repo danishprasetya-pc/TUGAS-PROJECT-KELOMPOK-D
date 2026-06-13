badc1397f8fb87546f0f0c45a1dab0c35ba2dfc5
nizaraflah <nizar.aflah@mhs.unsoed.ac.id>
2026-06-14 00:49:58 +0700

    Update 05_cluster_3d.R

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -1,14 +1,7 @@
-
-x1 <- as.numeric
-x2 <- as.numeric
-x3 <- as.numeric
-
-cluster_titik <- function(x1,x2,x3) {
-  
-  A <- c(2, 1, 3)
-  B <- c(1, -4, 6)
-  C <- c(-2, 3, -2)
-  
+cluster_titik <- function(x1, x2, x3) {
+  A <- c(3, 2, 4)
+  B <- c(-2, 5, 1)
+  C <- c(1, -3, -2)
   
   U <- c(x1, x2, x3)
   
@@ -16,15 +9,27 @@ cluster_titik <- function(x1,x2,x3) {
   dB <- sqrt(sum((U - B)^2))
   dC <- sqrt(sum((U - C)^2))
   
-  if (dA <= dB && dA <= dC) {
-    cat("Titik termasuk Cluster A\n")
-  } else if (dB <= dA && dB <= dC) {
-    cat("Titik termasuk Cluster B\n")
-  } else {
-    cat("Titik termasuk Cluster C\n")
+  print(paste("Jarak ke A:", round(dA, 4)))
+  print(paste("Jarak ke B:", round(dB, 4)))
+  print(paste("Jarak ke C:", round(dC, 4)))
+  
+  jarak_min <- min(dA, dB, dC)
+  cluster_terpilih <- c()
+  
+  if (dA == jarak_min) {
+    cluster_terpilih <- c(cluster_terpilih, "Cluster A")
   }
+  if (dB == jarak_min) {
+    cluster_terpilih <- c(cluster_terpilih, "Cluster B")
+  }
+  if (dC == jarak_min) {
+    cluster_terpilih <- c(cluster_terpilih, "Cluster C")
+  }
+  
+  cat("Titik termasuk", paste(cluster_terpilih, collapse = " dan "), "\n\n")
 }
 
-cluster_titik(1,2,3)
-cluster_titik(9,-7,3)
-cluster_titik(-5,8,-1)
+#Contoh pemanggilan fungsi
+cluster_titik(4, 3, 7)
+cluster_titik(3,-7,-1)
+cluster_titik(0.5, 3.5, 2.5)
