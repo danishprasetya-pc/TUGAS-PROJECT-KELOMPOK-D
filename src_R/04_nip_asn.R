615b5c336ac7d3f9fa60d3365d6620c2d5af7431
nizaraflah <nizar.aflah@mhs.unsoed.ac.id>
2026-06-14 00:47:44 +0700

    Update 04_nip_asn.R

--- src_R/04_nip_asn.R
+++ src_R/04_nip_asn.R
@@ -6,36 +6,18 @@ tanggal_lahir <- function(nip) {
   bulan <- as.numeric(substr(nip_str, 5, 6))
   tanggal <- as.numeric(substr(nip_str, 7, 8))
   
-  if (bulan == 1) {
-    nama_bulan <- "Januari"
-  } else if (bulan == 2) {
-    nama_bulan <- "Februari"
-  } else if (bulan == 3) {
-    nama_bulan <- "Maret"
-  } else if (bulan == 4) {
-    nama_bulan <- "April"
-  } else if (bulan == 5) {
-    nama_bulan <- "Mei"
-  } else if (bulan == 6) {
-    nama_bulan <- "Juni"
-  } else if (bulan == 7) {
-    nama_bulan <- "Juli"
-  } else if (bulan == 8) {
-    nama_bulan <- "Agustus"
-  } else if (bulan == 9) {
-    nama_bulan <- "September"
-  } else if (bulan == 10) {
-    nama_bulan <- "Oktober"
-  } else if (bulan == 11) {
-    nama_bulan <- "November"
-  } else if (bulan == 12) {
-    nama_bulan <- "Desember"
-  } else {
-    nama_bulan <- "Bulan Tidak Valid"
-  }
+  daftar_bulan <- c("Januari", "Februari", "Maret", "April", "Mei", "Juni", 
+                    "Juli", "Agustus", "September", "Oktober", "November", "Desember")
   
-  cat("Tanggal lahir ASN:", tanggal, nama_bulan, tahun, "\n")
+  if (bulan >= 1 && bulan <= 12) {
+    nama_bulan <- daftar_bulan[bulan]
+    cat("Tanggal lahir ASN:", tanggal, nama_bulan, tahun, "\n")
+  } else {
+    cat("Bulan Tidak Valid\n")
+  }
 }
 
-tanggal_lahir(199903272025062013)
-
+#Contoh pemanggilan fungsi
+tanggal_lahir(199210252015032015)
+tanggal_lahir(198540122010011003)
+tanggal_lahir(199812012023021001)
