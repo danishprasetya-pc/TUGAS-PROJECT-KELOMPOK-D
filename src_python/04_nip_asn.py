1d664d90dcd8c4fceb4ec37962998dfc1c3d0a26
nizaraflah <nizar.aflah@mhs.unsoed.ac.id>
2026-06-14 01:12:22 +0700

    Update 04_nip_asn.py

--- src_python/04_nip_asn.py
+++ src_python/04_nip_asn.py
@@ -1,40 +1,33 @@
-#Soal 4 Membuat Program untuk Menampilkan Tanggal Lahir ASN bila diberikan NIP ASN
 def tanggal_lahir(nip):
-
     nip_str = str(nip)
 
     tahun = int(nip_str[0:4])
     bulan = int(nip_str[4:6])
     tanggal = int(nip_str[6:8])
 
-    if bulan == 1:
-        nama_bulan = "Januari"
-    elif bulan == 2:
-        nama_bulan = "Februari"
-    elif bulan == 3:
-        nama_bulan = "Maret"
-    elif bulan == 4:
-        nama_bulan = "April"
-    elif bulan == 5:
-        nama_bulan = "Mei"
-    elif bulan == 6:
-        nama_bulan = "Juni"
-    elif bulan == 7:
-        nama_bulan = "Juli"
-    elif bulan == 8:
-        nama_bulan = "Agustus"
-    elif bulan == 9:
-        nama_bulan = "September"
-    elif bulan == 10:
-        nama_bulan = "Oktober"
-    elif bulan == 11:
-        nama_bulan = "November"
-    elif bulan == 12:
-        nama_bulan = "Desember"
+    daftar_bulan = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+    ]
+
+    if 1 <= bulan <= 12:
+        nama_bulan = daftar_bulan[bulan - 1]
+        print(f"Tanggal lahir ASN: {tanggal} {nama_bulan} {tahun}\n")
     else:
-        nama_bulan = "Bulan Tidak Valid" 
+        print("Bulan Tidak Valid\n")
 
-    print(f"Tanggal lahir ASN: {tanggal} {nama_bulan} {tahun}")
 
-tanggal_lahir(199012080000008866)
-
+#Contoh pemanggilan fungsi
+tanggal_lahir(199210252015032015)
+tanggal_lahir(198540122010011003)
+tanggal_lahir(199812012023021001)
