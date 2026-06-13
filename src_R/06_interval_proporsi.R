1c7ba1bedc1b72bf60ae0731d7fe91e878fee70e
nizaraflah <nizar.aflah@mhs.unsoed.ac.id>
2026-06-14 00:59:17 +0700

    Update 06_interval_proporsi.R

--- src_R/06_interval_proporsi.R
+++ src_R/06_interval_proporsi.R
@@ -1,12 +1,8 @@
 interval_konfidensi <- function(p_hat, n, alpha) {
-  
-  # Validasi proporsi
   if (p_hat < 0 || p_hat > 1) {
     cat("Error: Proporsi harus berada pada rentang 0 sampai 1.\n")
     return()
   }
-  
-  # Menentukan nilai z berdasarkan alpha
   if (alpha == 0.05) {
     z <- 1.96
   } else if (alpha == 0.10) {
@@ -16,25 +12,17 @@ interval_konfidensi <- function(p_hat, n, alpha) {
     return()
   }
   
-  # Menghitung margin of error
   margin_error <- z * sqrt((p_hat * (1 - p_hat)) / n)
-  
-  # Menghitung batas bawah interval
   batas_bawah <- p_hat - margin_error
-  
-  # Menghitung batas atas interval
   batas_atas <- p_hat + margin_error
-  
-  # Menampilkan hasil
+
   cat("Proporsi Sampel =", p_hat, "\n")
   cat("Ukuran Sampel =", n, "\n")
   cat("Alpha =", alpha, "\n")
-  cat("Interval Konfidensi = (",
-      round(batas_bawah, 3),
-      ",",
-      round(batas_atas, 3),
-      ")\n")
+  cat("Interval Konfidensi = (",round(batas_bawah, 3),",",round(batas_atas, 3),")\n")
 }
 
-# Contoh data
-interval_konfidensi(0.60, 100, 0.05)
+#Contoh pemanggilan fungsi
+interval_konfidensi(0.4, 100, 0.05)
+interval_konfidensi(0.6, 250, 0.01)
+interval_konfidensi(1.2, 150, 0.10)
